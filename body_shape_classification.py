b0433a1975b779b6783fd7d9b11bf02ab44fa113
RizwanAliQau <rizwanaliqau@gmail.com>
2024-02-16 00:47:38 +0900

    added all project files for body shape classification

---
+++ body_shape_classification.py
@@ -0,0 +1,97 @@
+import ultralytics 
+from ultralytics import YOLO
+import wandb
+import cv2
+import os
+wandb.disabled = True
+
+select = 'predict'
+
+
+if __name__ == '__main__':
+    if select == 'train':
+        # ============ TRAIN CODE ================= last training dir is rins/detect/train28 
+        model = YOLO('yolov8s.yaml') #runs/detect/YOLO_new_weights/weights/best.pt')
+        model.train(data='cfg/body_shape_classification.yaml', epochs=1000, imgsz=256, batch=32, patience=0)        
+    elif select == 'predict':
+        ############ DETECT PORTION ############################
+        model = YOLO('final_body_classification_/weights/best.pt')
+        path = "test_img/inverted_traiangle_women_0001.png" # dataset_ket_final/yolo_data/Connector_reference/train/images" # small_connectors_molds_white/normal/test/te    st_examples' #train_mold_mixed_data/val/images'
+        model.predict(source=path, show=False, conf=0.1, save=True, batch=1,save_crop=False)
+
+    elif select == 'vis':
+        # ============ DEMO CODE =======================
+        # Load the YOLOv8 model
+        model = YOLO('final_body_classification_/weights/best.pt') #RTDETR("rtdetr-l.pt")
+        # Open the video file
+        video_path = "DATA/mold_anom_norm.mp4"
+        cap = cv2.VideoCapture(video_path)
+
+        # Loop through the video frames
+        while cap.isOpened():
+            # Read a frame from the video
+            success, frame = cap.read()
+
+            if success:
+                # Run YOLOv8 inference on the frame
+                results = model.predict(frame) 
+
+                # Visualize the results on the frame
+                annotated_frame = results[0].plot()
+
+                # Display the annotated frame
+                cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+                # Break the loop if 'q' is pressed
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+            else:
+                # Break the loop if the end of the video is reached
+                break
+
+        # Release the video capture object and close the display window
+        cap.release()
+        cv2.destroyAllWindows()
+        print('End of prgramme !!!')
+    elif select=='camera':
+        # define a video capture object 
+        vid = cv2.VideoCapture(0) 
+        model = YOLO('final_body_classification_/weights/best.pt')
+        while(True): 
+            
+            # Capture the video frame 
+            # by frame 
+            ret, frame = vid.read() 
+            # Run YOLOv8 inference on the frame
+            results = model.predict(frame) 
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # Display the annotated frame
+            cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+            # Display the resulting frame 
+            #cv2.imshow('frame', frame) 
+            
+            # the 'q' button is set as the 
+            # quitting button you may use any 
+            # desired button of your choice 
+            if cv2.waitKey(1) & 0xFF == ord('q'): 
+                break
+        
+        # After the loop release the cap object 
+        vid.release() 
+        # Destroy all the windows 
+        cv2.destroyAllWindows() 
+    else:
+        raise NotImplementedError("Selection was wrong!") 
+        
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,55 @@
+import cv2
+
+def add_text(image, text='',pos=(10,10),color = (0, 0, 255)):
+    # font 
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+    
+    # org 
+    org = (00, 185) 
+    
+    # fontScale 
+    fontScale = 1
+
+    # Line thickness of 2 px 
+    thickness = 1
+    
+    # Using cv2.putText() method 
+    image = cv2.putText(image, text, pos, font, fontScale,  
+                    color, thickness, cv2.LINE_AA, False) 
+    return image
+
+
+
+def result_to_condition(class_id,annotated_frame):
+    if class_id==0:
+        annotated_frame =   add_text(annotated_frame,'***** Clothes Recommendations: A-line Silhouttes, flowy tunics, V-necks, fitted sleeves, trench-style coats, wrap dress, skinny jeans *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: Fitted or Structured Tops, Bright Color Tops, V-necks, Jackets, Dark Colored Pants, Necklaces, Earrings *****',pos=(50,50))
+    elif class_id==1:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: form fitting jersey knits, wrap tops, peplum blouses, V-necks, fitted blazers, wrap dress, stretchy skinny jeans, fitted jumpsuits *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: shapeless clothing, smock dresses, turtle-necks, drop waist *****',pos=(50,50)) 
+    elif class_id==2:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: weighty fabrics, tapered sleeves, wide straps, darker colored shirts, fitted and past-hip shirts, straight cut jackets, baggy trousers *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: boat necks, V-necks, heavy shoulder pads, skinny trousers *****',pos=(50,50)) 
+    elif class_id==3:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: Slim-fit shirts, Jeans, Straight-leg trousers, Slim-fit Jackets, Horizontal stripes, Trousers with larger seat drop *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: Structured tailoring, prints, patterns, scoop necklines *****',pos=(50,50)) 
+    elif class_id==4:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: slightly fitted pants, oversized sweaters, structured jackets, blazers, dark solid colors, fabrics with small patterns *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: horizontal patterns, stripes, tight clothing *****',pos=(50,50)) 
+    elif class_id==5:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: fitted or structured tops, bright colored tops, V-necks, jackets, dark colored pants, necklaces, earrings *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: Tight tops, hip-length&lowrise jackets, skinny jeans, leggings *****',pos=(50,50)) 
+    elif class_id==6:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: halter styles, peacoats, dusters, flowy outerwear, Empire line dresses, princess seam dresses, and wrap dresses, belts *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: shapeless and boxy styles, pea coats, baggy jeans *****',pos=(50,50)) 
+    elif class_id==7:
+        annotated_frame =   add_text(annotated_frame,'***** Clothes Recommendations: Horizontal stripes, Structurec *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: Double-Breasted Jackets *****',pos=(50,50))
+    elif class_id==8:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: Close-fit trousers, blaze and suits, vertical striped shirts, Normal lengthed neckties, check and plaid shirts *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: Loose and saggy clothing, bland clothing *****',pos=(50,50)) 
+    elif class_id==9:
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes Recommendations: Tailored patterned blazers, Vertical stripes, Jackets with structured shoulders, Single-breasted suits, Brighter color panels *****',color = (0, 255, 0))
+        annotated_frame =   add_text(annotated_frame, text='***** Clothes to Avoid: Fitted polo shirts, Roll necks, Oversized clothing, Skinny fits *****',pos=(50,50)) 
+    
+    return annotated_frame
